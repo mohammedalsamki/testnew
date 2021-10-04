75284a359184582de4700c183c7ac6d082d789a5
mohammedalsamki <momosamak21@gmail.com>
2021-10-04 10:19:38 -0400

    solve

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,7 +4,7 @@ const axios =require('axios');
 const getDrink = async(req,res) =>{
     const url =`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic`
         let axiosRes = await axios.get(url);
-    let Data = axiosRes.data;
+    let Data = axiosRes.data.drinks;
     console.log(Data)
 res.send(Data)
 }
